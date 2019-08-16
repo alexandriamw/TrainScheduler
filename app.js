42e3b6c19a7322c9106c2eab70e0b1a3e052e817
alexandriamw <alexandriawagner84@gmail.com>
2019-08-15 22:01:34 -0500

    completed js, modified table to include thead and tbody

--- app.js
+++ app.js
@@ -1,62 +1,100 @@
-// Assume the following situations.
-​
-    // (TEST 1)
-    // First Train of the Day is 3:00 AM
-    // Assume Train comes every 3 minutes.
-    // Assume the current time is 3:16 AM....
-    // What time would the next train be...? (Use your brain first)
-    // It would be 3:18 -- 2 minutes away
-​
-    // (TEST 2)
-    // First Train of the Day is 3:00 AM
-    // Assume Train comes every 7 minutes.
-    // Assume the current time is 3:16 AM....
-    // What time would the next train be...? (Use your brain first)
-    // It would be 3:21 -- 5 minutes away
-​
-​
-    // ==========================================================
-​
-    // Solved Mathematically
-    // Test case 1:
-    // 16 - 00 = 16
-    // 16 % 3 = 1 (Modulus is the remainder)
-    // 3 - 1 = 2 minutes away
-    // 2 + 3:16 = 3:18
-​
-    // Solved Mathematically
-    // Test case 2:
-    // 16 - 00 = 16
-    // 16 % 7 = 2 (Modulus is the remainder)
-    // 7 - 2 = 5 minutes away
-    // 5 + 3:16 = 3:21
-​
-    // Assumptions
-    var tFrequency = 3;
-​
-    // Time is 3:30 AM
-    var firstTime = "03:30";
-​
-    // First Time (pushed back 1 year to make sure it comes before current time)
-    var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
-    console.log(firstTimeConverted);
-​
-    // Current Time
-    var currentTime = moment();
-    console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
-​
-    // Difference between the times
-    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-    console.log("DIFFERENCE IN TIME: " + diffTime);
-​
-    // Time apart (remainder)
-    var tRemainder = diffTime % tFrequency;
-    console.log(tRemainder);
-​
-    // Minute Until Train
-    var tMinutesTillTrain = tFrequency - tRemainder;
-    console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
-​
-    // Next Train
-    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
-    console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+const trainScheduleStore = localforage.createInstance({
+    name: "trainSchedulerApp",
+    version: 1.0,
+    storeName: "trainSchedulerStore"
+});
+
+const scheduleKey = "trains";
+const currentTime = moment();
+
+trainScheduleStore.getItem(scheduleKey).then(function (value) {
+    let trains;
+
+    if (value === null) {
+        trains = [];
+    } else {
+        trains = JSON.parse(value);
+    }
+
+    buildSchedule(trains);
+});
+
+function clearForm () {
+    const trainName = document.getElementById("trainName").value = "";
+    const destination = document.getElementById("destination").value = "";
+    const firstTrainTime = document.getElementById("firstTrainTime").value = "";
+    const frequency = document.getElementById("frequency").value = "";
+}
+
+function buildSchedule (trains) {
+    const trainScheduleEl = document.getElementById("trainSchedule");
+    trainScheduleEl.innerHTML = "";
+
+    for (let trainIndex in trains) {
+        const train = trains[trainIndex];
+        const scheduleRowEl = document.createElement("tr");
+        const diffTime = moment().diff(moment(train.firstTrainTime), "minutes");
+        const minutesTillTrain = train.frequency - (diffTime % train.frequency);
+
+        const trainNameCellEl = document.createElement("td");
+        trainNameCellEl.textContent = train.trainName;
+        scheduleRowEl.appendChild(trainNameCellEl);
+
+        const destinationCellEl = document.createElement("td");
+        destinationCellEl.textContent = train.destination;
+        scheduleRowEl.appendChild(destinationCellEl);
+
+        const frequencyCellEl = document.createElement("td");
+        frequencyCellEl.textContent = train.frequency;
+        scheduleRowEl.appendChild(frequencyCellEl);
+
+        const nextArrivalCellEl = document.createElement("td");
+        nextArrivalCellEl.textContent = moment(moment().add(minutesTillTrain, "minutes")).format("hh:mm");
+        scheduleRowEl.appendChild(nextArrivalCellEl);
+
+        const minutesAwayCellEl = document.createElement("td");
+        minutesAwayCellEl.textContent = minutesTillTrain;
+        scheduleRowEl.appendChild(minutesAwayCellEl);
+
+        trainScheduleEl.appendChild(scheduleRowEl);
+    }
+}
+
+document.getElementById("addTrainForm").addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const trainName = document.getElementById("trainName").value;
+    const destination = document.getElementById("destination").value;
+    const firstTrainTime = document.getElementById("firstTrainTime").value;
+    const frequency = parseInt(document.getElementById("frequency").value);
+
+    if (trainName.length === 0 || destination.length === 0 || firstTrainTime.length === 0 || (frequency === 0 || frequency === NaN)) {
+        return false;
+    }
+
+    const trainObject = {
+        trainName: trainName,
+        destination: destination,
+        firstTrainTime: moment(firstTrainTime, "HH:mm").subtract(1, "years"),
+        frequency: frequency
+    };
+
+    trainScheduleStore.getItem(scheduleKey).then(function (value) {
+        let trains;
+
+        if (value === null) {
+            trains = [];
+        } else {
+            trains = JSON.parse(value);
+        }
+
+        trains.push(trainObject);
+
+        trainScheduleStore.setItem(scheduleKey, JSON.stringify(trains)).then(function () {
+            buildSchedule(trains);
+            clearForm();
+        }).catch(function (error) {
+            console.warn(error);
+        });
+    });
+});
